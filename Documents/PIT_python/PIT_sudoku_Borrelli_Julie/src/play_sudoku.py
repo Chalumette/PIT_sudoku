590649ef1a02e4676f501073f2daf1ae86cc9410
Julie <borrelli.julie1104@gmail.com>
2020-10-07 20:15:00 +0200

    Débogage play_sudoku

--- Documents/PIT_python/PIT_sudoku_Borrelli_Julie/src/play_sudoku.py
+++ Documents/PIT_python/PIT_sudoku_Borrelli_Julie/src/play_sudoku.py
@@ -2,8 +2,7 @@
 #Script permettant de jouer au sudoku et d'implanter le sudoku à la ligne n d'un fichier, pour cela, entrer en argument de ligne de commande : nom_fichier, numero_ligne
 
 import sys
-from Documents.PIT_python.PIT_sudoku_Borrelli_Julie.src.grid import SudokuGrid
-
+from grid import *
 
 def entre09 (valeurs):
 	"""Vérifie que les valeurs sont comprises entre 1 et 9"""
@@ -12,7 +11,7 @@ def entre09 (valeurs):
 		if not 1<=i<=9:
 			inter=False
 			break
-	return inter
+	return (inter)
 
 
 def remplissage(sudoku):
@@ -31,12 +30,14 @@ def remplissage(sudoku):
 			sudoku.write(info[0],info[1],info[2])
 ################
 #On regarde si des paramètres ont été rentrés en ligne de commande
-param=True if len(sys.argv)>0 
+param=True if len(sys.argv)>1 else False
+print(sys.argv)
 #On crée une instance de la grille en fonction à l'aide des méthodes de classe.
+
 if not param:
 	grille=SudokuGrid.from_stdin()
 else:
-	grille=SudokuGrid.from_file(sys.argv[0], sys.argv[1])
+	grille=SudokuGrid.from_file(sys.argv[1], int(sys.argv[2]))
 #Remplissage du sudoku
 while len(grille.get_empty_positions())!=0:
 	remplissage(grille)
